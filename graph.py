6718492cd21a15e539c3d72dcfece288c5eccd15
Fahiye Muhammad <fahiye254@gmail.com>
2026-04-15 17:03:32 +0300

    Added an option for changing the language

--- graph.py
+++ graph.py
@@ -3,6 +3,7 @@ from joke import show_menu
 from joke import update_category
 from joke import exit_bot
 from joke import route_choice
+from joke import update_language
 from joke import JokeState
 from langgraph.graph import StateGraph, END, START
 from langgraph.graph.state import CompiledStateGraph
@@ -15,6 +16,7 @@ def build_joke_graph() -> CompiledStateGraph:
     workflow.add_node("show_menu", show_menu)
     workflow.add_node("fetch_joke", fetch_joke)
     workflow.add_node("update_category", update_category)
+    workflow.add_node("update_language", update_language)
     workflow.add_node("exit_bot", exit_bot)
 
     workflow.set_entry_point("show_menu")
@@ -25,12 +27,14 @@ def build_joke_graph() -> CompiledStateGraph:
         {
             "fetch_joke": "fetch_joke",
             "update_category": "update_category",
+            "update_language": "update_language",
             "exit_bot" : "exit_bot",
         }
     )
 
     workflow.add_edge("fetch_joke", "show_menu")
     workflow.add_edge("update_category", "fetch_joke")
+    workflow.add_edge("update_language", "fetch_joke")
     workflow.add_edge("exit_bot", END)
 
     return workflow.compile()
--- joke.py
+++ joke.py
@@ -19,7 +19,7 @@ class Joke(BaseModel):
 
 class JokeState(BaseModel):
     jokes: Annotated[List[Joke], add] = []
-    jokes_choice: Literal["n", "c", "q"] = "n"
+    jokes_choice: Literal["n", "c", "l" ,"q"] = "n"
     category:str = "neutral"
     language : str = "en"
     quit: bool = False
@@ -27,10 +27,10 @@ class JokeState(BaseModel):
 
 def show_menu(state:JokeState) -> dict:
     while True:
-        user_input = input("[n] Next  [c] Category  [q] Quit\n> ").strip().lower()
-        if user_input in ["n", "c", "q"]:
+        user_input = input("[n] Next  [c] Category [l] Language [q] Quit\n> ").strip().lower()
+        if user_input in ["n", "c", "q", "l"]:
             return {"jokes_choice": user_input}  
-        print("❌  Wrong input! Please enter n, c, or q.   ❌")
+        print("❌  Wrong input! Please enter n, c, l, or q.   ❌")
 
 def fetch_joke(state: JokeState) -> dict:
     joke_text = get_joke(language=state.language, category=state.category)
@@ -47,6 +47,11 @@ def update_category(state: JokeState):
     selection = int(input("Select category [0=neutral, 1=chuck, 2=all]: ").strip())
     return {"category": categories[selection]}
 
+def update_language(state: JokeState):
+    languages = ["en", "de", "es"]
+    selection = int(input("Select language [0=en, 1=de, 2=es]: ").strip())
+    return {"language": languages[selection]}
+
 def exit_bot(state: JokeState) -> dict:
     return {"quit": True}
 
@@ -55,6 +60,8 @@ def route_choice(state: JokeState)  -> str:
         return "fetch_joke"
     elif state.jokes_choice == 'c':
         return "update_category"
+    elif state.jokes_choice == "l":
+        return "update_language"
     elif state.jokes_choice == 'q':
         return "exit_bot"
     return "exit_bot"
